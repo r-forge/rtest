0472122c94ff807c74cc4d180da45bb14281c6da
mariotomo (drs ) <mariotomo@gmail.com>
2010-09-22 09:43:21 +0000

    added the backends, for base and system.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rtest@5 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/basesuite.r
@@ -0,0 +1,189 @@
+##  RUnit : A unit test framework for the R programming language
+##  Copyright (C) 2003-2009  Thomas Koenig, Matthias Burger, Klaus Juenemann
+##
+##  This program is free software; you can redistribute it and/or modify
+##  it under the terms of the GNU General Public License as published by
+##  the Free Software Foundation; version 2 of the License.
+##
+##  This program is distributed in the hope that it will be useful,
+##  but WITHOUT ANY WARRANTY; without even the implied warranty of
+##  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##  GNU General Public License for more details.
+##
+##  You should have received a copy of the GNU General Public License
+##  along with this program; if not, write to the Free Software
+##  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+##  $Id$
+
+## this file describes the generic functions and implements the
+## .default and .RTestSuite implementations.  each derived class is
+## described in a separate file.
+
+## in the derived classes please try to follow the order: creator,
+## private functions (here not present), overriden methods (here:
+## methods to override), public utility functions (here: base methods
+## that don't need overriding).
+
+baseTestSuite <- function(name, dirs, 
+                          rngKind="Marsaglia-Multicarry",
+                          rngNormalKind="Kinderman-Ramage", ...) {
+  
+  if (missing(dirs)) {
+    stop("argument 'dirs' is missing without a default.")
+  }
+  if (missing(name)) {
+    warning("argument 'name' is missing. using basename(dirs)[1] instead.")
+    name <- basename(dirs)
+  }
+
+  ret <- list(name=name,
+              dirs=dirs,
+              rngKind=rngKind,
+              rngNormalKind=rngNormalKind, ...)
+  class(ret) <- c('RBaseTestSuite', class(ret))
+  invisible(ret)
+}
+
+### --------------------------------------------------------------------
+### isValidTestSuite
+isValidTestSuite <- function(self)
+  UseMethod('isValidTestSuite')
+
+isValidTestSuite.default <- function(self) {
+  warning(paste("'testSuite' object is not of class 'RUnitTestSuite'."))
+  return(FALSE)
+}
+
+isValidTestSuite.RBaseTestSuite <- function(self) {
+  for(i in seq_along(self))
+  {
+    if(!is.character(self[[i]]))
+    {
+      warning(paste("'self' object does not conform to S3 class definition.\n",
+                    names(self)[i], " element has to be of type 'character'."))
+      return(FALSE)
+    }
+    if(self[[i]] == "")
+    {
+      warning(paste("'self' object does not conform to S3 class definition.\n",
+                    names(self)[i], " element may not be empty string."))
+      return(FALSE)
+    }
+  }
+  
+  if (!all(file.exists(self[["dirs"]])))
+  {
+    warning(paste("specified directory", paste(self[["dirs"]], collapse=", "), "not found."))
+    return(FALSE)
+  }
+
+  if (length(self[["name"]]) != 1) {
+    warning(paste("'name' element may only contain exactly one name."))
+    return(FALSE)
+  }
+  return(TRUE)
+}
+
+### --------------------------------------------------------------------
+### runSingleValidTestSuite
+runSingleValidTestSuite <- function(self)
+  UseMethod('runSingleValidTestSuite')
+
+runSingleValidTestSuite.RBaseTestSuite <- function(self) {
+  .testLogger$addError("don't know how to run a base test suite.")
+}
+
+### --------------------------------------------------------------------
+### runTestSuite
+runTestSuite <- function(testSuite, ...)
+  UseMethod('runTestSuite')
+
+runTestSuite.default <- function(testSuite, ...)
+  .testLogger$addError("runTestSuite invoked on a generic object.")
+
+runTestSuite.list <- function(testSuite, ...) {
+  ## testSuite is actually a list of suites
+  for(i in seq_along(testSuite))
+    runTestSuite(testSuite[[i]], ...)
+}
+
+runTestSuite.RBaseTestSuite <- function(testSuite, ..., useOwnErrorHandler=TRUE, verbose=getOption("RUnit")$verbose) {
+  ##@bdescr
+  ## This is the main function of the RUnit framework. It identifies all specified
+  ## test files and triggers all required actions. At the end it creates a test
+  ## protocol data object. 
+  ## IMPORTANT to note, the random number generator is (re-)set to the default
+  ## methods specified in defineTestSuite() before each new test case *file* is sourced. 
+  ## This guarantees that each new test case set defined together in on file can rely
+  ## on the default, even if the random number generator version is being reconfigured in some
+  ## previous test case file(s).
+  ##@edescr
+  ##
+  ##@in  testSuite         : [list] list of test suite lists
+  ##@in  useOwnErrorHandler : [logical] TRUE (default) : use the RUnit error handler
+  ##@in  verbose            : [integer] >= 1: (default) write begin/end comments for each test case, 0: omit begin/end comment 
+  ##@ret                    : [list] 'RUnitTestData' S3 class object
+  ##
+  ##@codestatus : testing
+  
+  ##  preconditions
+  if (!is.logical(useOwnErrorHandler)) {
+    stop("argument 'useOwnErrorHandler' has to be of type logical.")
+  }
+  if (length(useOwnErrorHandler) != 1) {
+    stop("argument 'useOwnErrorHandler' has to be of length 1.")
+  }
+  if (is.na(useOwnErrorHandler)) {
+    stop("argument 'useOwnErrorHandler' may not contain NA.")
+  }
+
+  oFile <- getOption("RUnit")$outfile
+  if (!is.null(oFile)) {
+    if(is.character(oFile)) {
+      ##  connection has to be open when handed on to sink
+      oFile <- file(oFile, "w")
+    } else if(!inherits(oFile, "connection")) {
+      stop("'outfile' must be a connection or a character string.")
+    }
+    sink(file=oFile)
+    sink(file=oFile, type="message")
+    resetStream <- function() {
+      sink(type="message")
+      sink()
+      flush(oFile)
+      close(oFile)
+      ##close(oFile)
+    }
+    on.exit(resetStream())
+  }
+  ##  record RNGkind and reinstantiate on exit
+  rngDefault <- RNGkind()
+  on.exit(RNGkind(kind=rngDefault[1], normal.kind=rngDefault[2]), add=TRUE)
+  
+  oldErrorHandler <- getOption("error")
+  ## reinstall error handler
+  on.exit(options(error=oldErrorHandler), add=TRUE)
+  
+  ## initialize TestLogger
+  assign(".testLogger", .newTestLogger(useOwnErrorHandler), envir=.GlobalEnv)
+  .testLogger$setVerbosity(verbose)
+
+  inputTestSuite <- testSuite
+  ## main loop
+  for(i in seq_along(inputTestSuite$dirs)) {
+    testSuite$name <- paste(inputTestSuite$name, i, sep='.')
+    testSuite$dirs <- inputTestSuite$dirs[[i]]
+    if(!isValidTestSuite(testSuite)) {
+      errMsg <- paste("Invalid test suite", testSuite$name, ". Test run skipped.")
+      .testLogger$addError(testSuite$name, errMsg)
+    }
+    .testLogger$setCurrentTestSuite(testSuite)
+    runSingleValidTestSuite(testSuite)
+  }
+
+  ret <- .testLogger$getTestData()
+  
+  return(ret)
+}
+
---
+++ pkg/R/systemtest.r
@@ -0,0 +1,174 @@
+##  RUnit : A unit test framework for the R programming language
+##  Copyright (C) 2003-2010 Thomas Koenig, Matthias Burger, Klaus
+##  Juenemann, Mario Frasca
+##
+##  This program is free software; you can redistribute it and/or modify
+##  it under the terms of the GNU General Public License as published by
+##  the Free Software Foundation; version 2 of the License.
+##
+##  This program is distributed in the hope that it will be useful,
+##  but WITHOUT ANY WARRANTY; without even the implied warranty of
+##  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##  GNU General Public License for more details.
+##
+##  You should have received a copy of the GNU General Public License
+##  along with this program; if not, write to the Free Software
+##  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+##  $Id$
+
+systemTestSuite <- function(name, dirs, 
+                            rngKind="Marsaglia-Multicarry",
+                            rngNormalKind="Kinderman-Ramage",
+                            test.dir='../tests/',
+                            scripts.regex='.+\\.[rR]$',
+                            case.in.pattern='%S-%T.in',
+                            case.out.pattern='%S-%T.out',
+                            name.in.match='read.PI',
+                            name.out.match='write.PI',
+                            ... ) {
+  ##@bdescr
+  ##  constructor function for system test suites
+
+  ret <- baseTestSuite(name, dirs, rngKind, rngNormalKind, 
+                       test.dir=test.dir, scripts.regex=scripts.regex,
+                       case.in.pattern=case.in.pattern, case.out.pattern=case.out.pattern,
+                       name.in.match=name.in.match, name.out.match=name.out.match, ...)
+  class(ret) <- c("RSystemTestSuite", class(ret))
+  invisible(ret)
+}
+
+extractMatching <- function(lines, perl.regex, preselect=NULL) {
+  if(!is.null(preselect))
+    lines <- lines[grep(preselect, lines, perl=TRUE)]
+  found <- regexpr(perl.regex, lines, perl=TRUE)
+  match.length <- attributes(found)$match.length[found != -1]
+  start.pos <- found[found != -1]
+  return(substr(lines[found != -1], start.pos, start.pos + match.length - 1))
+}
+
+patternToRegex <- function(case.in.pattern, basename.script, testCaseName=NULL, perl=FALSE) {
+  pattern <- sub('%S', basename.script, case.in.pattern, fixed=TRUE)
+  parts <- strsplit(pattern, "%T", fixed=TRUE)[[1]]
+  if(!is.null(testCaseName))
+    return(paste(parts[1], testCaseName, parts[2], sep=""))
+  if(isTRUE(perl))
+    return(paste("(?<=", parts[1], ").*?(?=", parts[2], ")", sep=""))
+  else
+    return(paste(parts[1], ".*?", parts[2], sep=""))
+}
+
+isValidTestSuite.RSystemTestSuite <- function(self) {
+  requiredNames <-  c("name", "dirs", "test.dir", "scripts.regex", "case.in.pattern",
+                      "case.out.pattern", "name.in.match","name.out.match",
+                      "rngKind", "rngNormalKind")
+  if(!all(requiredNames %in% names(self)))
+  {
+    warning("'self' object does not conform to S3 class definition. Not all list elements present.")
+    return(FALSE)
+  }
+
+  if(!NextMethod(.Generic))
+    return(FALSE)
+  
+  if(!(length(self[['dirs']]) == 1)) {
+    warning(paste("length of 'dirs' element must be 1. --so sorry--."))
+    return(FALSE)
+  }
+  
+  lengths <- c(1, length(self$dirs))
+  if(!(length(self[['test.dir']]) %in% lengths)) {
+    warning(paste("length of 'test.dir' element must be 1 or match 'dirs'."))
+    return(FALSE)
+  }
+  if(!all(file.exists(paste(self$dirs, self$test.dir, sep='/')))) {
+    warning(paste("not all test directories exist."))
+    return(FALSE)
+  }
+  if(!(length(self[['scripts.regex']]) %in% lengths)) {
+    warning(paste("length of 'scripts.regex' element must be 1 or match 'dirs'."))
+    return(FALSE)
+  }
+  if(!(length(self[['case.in.pattern']]) %in% lengths)) {
+    warning(paste("length of 'case.in.pattern' element must be 1 or match 'dirs'."))
+    return(FALSE)
+  }
+  if(!(length(self[['case.out.pattern']]) %in% lengths)) {
+    warning(paste("length of 'case.out.pattern' element must be 1 or match 'dirs'."))
+    return(FALSE)
+  }
+  return(TRUE)
+}
+
+runSingleValidTestSuite.RSystemTestSuite <- function(self) {
+  ## for each script (this means: let's build the vector, and loop)
+  scripts <- list.files(self$dirs,
+                        pattern=self$scripts.regex,
+                        full.names=TRUE)
+  for(script in scripts) {
+    .testLogger$setCurrentSourceFile(script)
+    ## get the expected input and output file names
+    lines <- readLines(script)
+    script <- basename(script)
+    name.in <- extractMatching(lines, "(?<=[\'\"]).*?(?=[\'\"])", preselect=self$name.in.match)
+    name.out <- extractMatching(lines, "(?<=[\'\"]).*?(?=[\'\"])", preselect=self$name.out.match)
+
+    ## get the basename of the script, without extension
+    parts <- strsplit(script, '.', fixed=TRUE)[[1]]
+    basename.script <- paste(parts[-length(parts)], collapse='.')
+
+    ## replace the basename in the case.in.pattern and construct the
+    ## corresponding regular expression, for use in list.files
+    case.in.regex <- patternToRegex(self$case.in.pattern, basename.script)
+    testFiles <- list.files(paste(self$dirs, self$test.dir, sep='/'),
+                            pattern=case.in.regex,
+                            full.names=TRUE)
+    if(length(testFiles) == 0) {
+      .testLogger$addDeactivated(testFuncName=script)
+    } else
+    for(case.in.name in testFiles) {
+      ## preparations
+      case.in.matcher <- patternToRegex(self$case.in.pattern, basename.script, perl=TRUE)
+      testName <- extractMatching(case.in.name, case.in.matcher)
+      case.out.name <- patternToRegex(self$case.out.pattern, basename.script, testName)
+      case.out.name <- paste(self$dirs, self$test.dir, case.out.name, sep='/')
+
+      ## sanity checks
+      if(!file.exists(case.in.name)) {
+        .testLogger$addError(paste("input for test case", testName, "not found.  skipping."))
+        next
+      }
+      if(!file.exists(case.out.name)) {
+        .testLogger$addError(paste("output for test case", testName, "not found.  skipping."))
+        next
+      }
+
+      ## copy stuff to expected place
+      file.copy(case.in.name, file.path(self$dirs, name.in), overwrite=TRUE)
+      
+      ## execute script in temporary environment
+      sandbox <- new.env()
+      pwd <- getwd()
+      setwd(self$dirs)
+      timing <- try(system.time(sys.source(script, sandbox)))
+      setwd(pwd)
+
+      ## check it did not crash
+      if(inherits(timing, 'try-error')) {
+        .testLogger$addError(testFuncName=paste(script, testName, sep=':'),
+                             errorMsg=geterrmessage())
+      } else {
+        ## check output (name.out compared to case.out.name)
+        target <- readLines(case.out.name)
+        current <- readLines(file.path(self$dirs, name.out))
+        if(any(current != target)) {
+          diff <- which(current != target)
+          msg <- paste(length(diff), " difference(s), namely at lines ", paste(diff, collapse=", "), ". ", sep="")
+          .testLogger$addFailure(testFuncName=testName, failureMsg=msg)
+        } else {
+          .testLogger$addSuccess(testFuncName=testName, secs=round(timing[3], 2))
+        }
+      }
+    }
+  }
+}
